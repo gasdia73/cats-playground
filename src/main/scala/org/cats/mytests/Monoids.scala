b3953bbc4a0d3aeff6a386c6d387b3ac2f3e35a8
gasdia73 <gasdia73@gmail.com>
2024-01-03 18:04:22 +0100

    monoids imported from cats

--- src/main/scala/org/cats/mytests/Monoids.scala
+++ src/main/scala/org/cats/mytests/Monoids.scala
@@ -2,23 +2,23 @@ package org.cats.mytests
 
 object Monoids extends App {
 
-  trait Semigroup[A] {
+  trait MySemigroup[A] {
     def combine(x: A, y: A): A
   }
 
-  trait Monoid[A] extends Semigroup[A] {
+  trait MyMonoid[A] extends MySemigroup[A] {
     def empty: A
   }
 
-  object Monoid {
-    def apply[A](implicit monoid: Monoid[A]) =
+  object MyMonoid {
+    def apply[A](implicit monoid: MyMonoid[A]) =
       monoid
   }
 
   //boolean monoids
 
-  val booleanSemigroup: Semigroup[Boolean] = (x, y) => x && y
-  val booleanMonoid: Monoid[Boolean] = new Monoid[Boolean] {
+  val booleanSemigroup: MySemigroup[Boolean] = (x, y) => x && y
+  val booleanMonoid: MyMonoid[Boolean] = new MyMonoid[Boolean] {
     override def empty: Boolean = true
 
     override def combine(x: Boolean, y: Boolean): Boolean = booleanSemigroup.combine(x, y)
@@ -31,11 +31,28 @@ object Monoids extends App {
   assert(booleanMonoid.combine(true, booleanMonoid.empty) == true)
 
   //sets monoids
-  val setSemigroup : Semigroup[Set[_]] = (s1, s2) => s1.concat(s2)
-  val setMonoid : Monoid[Set[_]] = new Monoid[Set[_]] {
+  val setSemigroup : MySemigroup[Set[_]] = (s1, s2) => s1.concat(s2)
+  val setMonoid : MyMonoid[Set[_]] = new MyMonoid[Set[_]] {
     override def empty: Set[_] = Set.empty
     override def combine(x: Set[_], y: Set[_]): Set[_] = setSemigroup.combine(x, y)
   }
 
+  import cats.instances.int._
+  import cats.syntax.semigroup._
 
+//
+//  def add(items: List[Int]): Int =
+//    items match {
+//      case x :: xs => x |+| add(xs)
+//      case _ => 0
+//    }
+
+  //or
+
+  import cats.Monoid
+
+  def add[A](items: List[A])(implicit monoid: Monoid[A]): A =
+    items.foldLeft(monoid.empty)(_ |+| _)
+
+  println(add(List(1,3,5,7)))
 }
